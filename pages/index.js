4092dacddfd45416c692f9705a0e02614298db11
YOUR NAME <YOUR@EMAIL.com>
2017-06-04 15:09:10 -0400

    testing firebase again

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,19 @@ import Connected from '../components/Connected';
 import Latest from '../components/Latest';
 import SocialHeaderIcons from '../components/Social';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import Document, { Head, Main, NextScript } from 'next/document'
-import flush from 'styled-jsx/server'
+import Document, { Head, Main, NextScript } from 'next/document';
+import flush from 'styled-jsx/server';
+import * as firebase from 'firebase';
 
+var config = {
+    apiKey: "AIzaSyAARIrGO5lYPQL1g6HzJNIhzQ3CXn5YD1Y",
+    authDomain: "portfolio-89edf.firebaseapp.com",
+    databaseURL: "https://portfolio-89edf.firebaseio.com",
+    projectId: "portfolio-89edf",
+    storageBucket: "portfolio-89edf.appspot.com",
+    messagingSenderId: "729287081290"
+  };
+firebase.initializeApp(config);
 
 
 
